1db308285449b64a66341b31fc895deb7747a38c
lyh <2256551717@qq.com>
2019-04-05 16:22:19 +0800

    Update sh1.c

--- 3_sh1/sh1.c
+++ 3_sh1/sh1.c
@@ -39,36 +39,48 @@ int cmdCheck(char* cmd){
 
 void exitShell(int flag, char* m){
     if (flag==0){
-		printf("exit()\n");
-	}
+            printf("exit()\n");
+    }
 	else if (m){
 		printf("\033[47;31mExit Error:\t%s\033[m\n", m);
-	}
+    }
 	else{
-		printf("\033[47;31mUnkown exit error !\033[m\n");
-	}
+        printf("\033[47;31mUnkown exit error !\033[m\n");
+    }
 }
 
 void mycd(char* command){
-    //printf("it's cd\n");
     char* sp;
     char* tmp;
+    char tmp2[101];
+    struct passwd* pw = NULL;
 
-    strtok_r(command, " ", &sp);
-    tmp = strtok_r(sp, " ", &sp);
-    if (tmp&&strlen(tmp)&&strcmp(tmp, "~")&&strcmp(tmp, "~/")){
-        if (chdir(tmp)){
-            printf("shell: cd: %s: No such file or directory\n", tmp);
+    pw = getpwuid(getuid());
+    if (pw){
+        //get homepath
+        char home[81] = "/home/";
+        strcat(home, pw->pw_name);
+        //get command path
+        strtok_r(command, " ", &sp);
+        tmp = strtok_r(sp, " ", &sp);
+
+        if (tmp&&strlen(tmp)){
+            strcpy(tmp2, tmp);
+            if ((sp = strchr(tmp2, '~'))&&*(sp+1)!='~'){
+                *sp = '\0';
+                strcat(tmp2, home);
+                strcat(tmp2, tmp+(sp-tmp2)+1);
+            }
+            if (chdir(tmp2)){
+                printf("shell: cd: %s: No such file or directory\n", tmp2);
+            }
         }
-    }
-    else{ //go to home
-        struct passwd* pw = NULL;
-        pw = getpwuid(getuid());
-        if (pw){
-            char home[81] = "/home/";
-            strcat(home, pw->pw_name);
+        else{ //go to homepath
             chdir(home);
         }
+    }
+    else {  //error
+        printf("Username Error!\n");
     }
 
     return;
@@ -89,13 +101,13 @@ int main(){
     char command[81];
     char cmd[81];
     int cmdID;
+	
+	//welcome
+	printf("\033[47;34m-----------------------------------------------------------\033[m\n");
+	printf("\033[47;34m|                welcome to lyh's shell                   |\033[m\n");
+	printf("\033[47;34m-----------------------------------------------------------\033[m\n");
 
-    //welcome
-    printf("\033[47;34m-----------------------------------------------------------\033[m\n");
-    printf("\033[47;34m|                welcome to lyh's shell                   |\033[m\n");
-    printf("\033[47;34m-----------------------------------------------------------\033[m\n");
-
-    //main cycle
+	//main cycle
     newCommand();
     while(cmdID = cmdCheck(cmd)){  //cmdID = 0 -> exit
         switch(cmdID){
@@ -113,17 +125,9 @@ int main(){
         }
         newCommand();
     }
-    /*
-    while(strcmp(command, "exit")){
-        if(mysys(command) == 65280){    //(-1)&0xFF00 = 65280
-            printf("shell: %s: command not found!\n", strtok(command, " "));
-        }
-        newCommand();
-        }
-    */
-
-    //exit
-	exitShell(exitFlag, message);
 	
+	//exit
+	exitShell(exitFlag, message);
+
 	return 0;
 }
